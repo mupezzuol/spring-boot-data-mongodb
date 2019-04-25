df0c393d7e0d12ff0bce6f7fef5c7486dc8c415c
mupezzuol <murillopezzuol@hotmail.com>
2019-04-25 19:54:09 -0300

    Subindo Projeto

---
+++ src/main/java/com/springbootdata/SpringBootDataMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataMongodbApplication.class, args);
+	}
+
+}
